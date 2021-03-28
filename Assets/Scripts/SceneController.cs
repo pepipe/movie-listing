1db94fee45db7d88da63c37967270288a005d8a7
felipe.cabedo <ffcabedo@gmail.com>
2021-03-28 03:22:03 +0100

    renamed ScreenController to SceneController

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,44 +1,52 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Parser;
 using TMPro;
 using UI;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScreenController : MonoBehaviour {
+public class SceneController : MonoBehaviour {
+    public delegate void ChangePageHander(int currPage, int totalPages);
+
+    public event ChangePageHander PrevPageEvent;
+    public event ChangePageHander NextPageEvent;
+
     [Tooltip("CSV file to load from Resources folder (without extension)")]
     [SerializeField] private string fileToLoad = "movie_metadata";
     [Tooltip("Container for the rows of data")]
     [SerializeField] private EntriesContainer parent = null;
     [SerializeField] private GameObject singleElementView = null;
     [SerializeField] private EntrySettings entrySettings = null;
-    [SerializeField] private int numberOfEntriesPerPage = 50;
+    [SerializeField] private int startPage = 0;
+    [SerializeField] private int entriesPerPage = 50;
 
     private IParser _parser;
     private IFileData _fileData;
     private EntriesPool _entriesPool;
-    private int _currStartIdx = 0;
-        
+    private int _currPage = 0;
+    private int _totalPages;
+
     private void Awake() {
         Debug.Assert(entrySettings != null, "Please assign an EntrySettings to SceneController");
-        _entriesPool = new EntriesPool(entrySettings, parent, singleElementView, numberOfEntriesPerPage);
+        _entriesPool = new EntriesPool(entrySettings, parent, singleElementView, entriesPerPage);
     }
 
     private void Start() {
         //Read data 
         _parser = new CsvParser();
         _fileData = _parser.ParseFromResources(fileToLoad);
-             
-        // //Build UI elements
-        _entriesPool.SetEntriesData(_fileData.GetHeaders(),
-                                    _fileData.GetEntries()
-                                                .Where((v, i) => 
-                                                        _currStartIdx <= i && i < _currStartIdx + numberOfEntriesPerPage)
-                                                .ToList());
-        _entriesPool.SetPoolActive(true);
-        // CreateData();
-    }   
+        _totalPages = _fileData.EntriesCount() % entriesPerPage == 0 ?
+                        _fileData.EntriesCount() / entriesPerPage : 
+                        (_fileData.EntriesCount() / entriesPerPage) + 1;
         
+        // //Build UI elements
+        _currPage = startPage;
+        PrevPageEvent.Invoke(_currPage, _totalPages);//to init page text and disable prev button
+        GetPage();
+        // CreateData();
+    }
+
     /// <summary>
     /// Used in button event
     /// </summary>
@@ -46,11 +54,40 @@ public class ScreenController : MonoBehaviour {
         singleElementView.SetActive(false);
     }
 
+    public void PrevPage() {
+        if (_currPage - 1 < 0) return;
+
+        --_currPage;
+        GetPage();
+        PrevPageEvent?.Invoke(_currPage, _totalPages);
+    }
+    
+    public void NextPage() {
+        if (_currPage + 1 >= _totalPages) return;
+
+        ++_currPage;
+        GetPage();
+        NextPageEvent?.Invoke(_currPage, _totalPages);
+    }
+
+    private void GetPage() {
+        _entriesPool.SetPoolActive(false);
+        var pageStartIdx = _currPage * entriesPerPage;
+        var pageEndIdx = Math.Min(pageStartIdx + entriesPerPage, _fileData.EntriesCount() - 1);
+        
+        _entriesPool.SetEntriesData(_fileData.GetHeaders(),
+            _fileData.GetEntries()
+                .Where((v, i) =>
+                    i >= pageStartIdx && i < pageEndIdx)
+                .ToList());
+    }
+    
+    //TODO: to remove this
     private void CreateData() {
         int headerIdx;
         string entryValue;
         GameObject entryGo;
-        for(var i = 0; i < numberOfEntriesPerPage; ++i){
+        for(var i = 0; i < entriesPerPage; ++i){
             entryGo = Instantiate(entrySettings.EntryPrefab, parent.transform);
             SetEntryButtonAction(entryGo.GetComponent<Button>());
             foreach (var header in entrySettings.HeadersToUse) {
